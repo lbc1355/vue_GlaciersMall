a59ce3dec14692ab647c0e5a245eb7ab9fc3fcbb
lioch3cooh <goodMorning_glb@outlook.com>
2021-12-06 17:27:51 +0800

    商品信息-地址选择

---
+++ src/components/library/xtx-city.vue
@@ -0,0 +1,188 @@
+<template>
+  <div class="xtx-city" ref="target">
+    <div class="select" @click="toggle" :class="{ active: visible }">
+      <span v-if="!fullLocation" class="placeholder">请选择配送地址</span>
+      <span v-else class="value">{{ fullLocation }}</span>
+      <i class="iconfont icon-angle-down"></i>
+    </div>
+    <div class="option" v-if="visible">
+      <span
+        class="ellipsis"
+        @click="changeItem(item)"
+        v-for="item in currList"
+        :key="item.code"
+        >{{ item.name }}</span
+      >
+    </div>
+  </div>
+</template>
+<script>
+import { computed, reactive, ref } from 'vue'
+import { onClickOutside } from '@vueuse/core'
+import axios from 'axios'
+export default {
+  name: 'XtxCity',
+  props: {
+    fullLocation: {
+      type: String,
+      default: ''
+    }
+  },
+  setup (props, { emit }) {
+    // 显示隐藏数据
+    const visible = ref(false)
+
+    // 所有省市区数据
+    const allCityData = ref([])
+    // 正在加载数据
+    const loading = ref(false)
+
+    const open = () => {
+      visible.value = true
+      // 获取地区数据
+      loading.value = true
+      getCityData().then(data => {
+        allCityData.value = data
+        loading.value = false
+      })
+      // 清空之前选择
+      for (const key in changeResult) {
+        changeResult[key] = ''
+      }
+    }
+
+    const close = () => {
+      visible.value = false
+    }
+
+    const toggle = () => {
+      visible.value ? close() : open()
+    }
+
+    const target = ref(null)
+    onClickOutside(target, () => {
+      close()
+    })
+
+    // 实现计算属性， 获取当前显示的地区数组
+    const currList = computed(() => {
+      // 默认默认省一级
+      let list = allCityData.value
+      //  可能市一级
+      if (changeResult.provinceCode && changeResult.provinceName) {
+        list = list.find(p => p.code === changeResult.provinceCode).areaList
+      }
+      if (changeResult.cityCode && changeResult.cityName) {
+        list = list.find(c => c.code === changeResult.cityCode).areaList
+      }
+      return list
+    })
+
+    // 定义选择的省市区数据
+    const changeResult = reactive({
+      provinceCode: '',
+      provinceName: '',
+      cityCode: '',
+      cityName: '',
+      countyCode: '',
+      countyName: '',
+      fullLocation: ''
+    })
+
+    const changeItem = (itme) => {
+      if (itme.level === 0) {
+        // 省级
+        changeResult.provinceCode = itme.code
+        changeResult.provinceName = itme.name
+      }
+      if (itme.level === 1) {
+        // 市级
+        changeResult.cityCode = itme.code
+        changeResult.cityName = itme.name
+      }
+      if (itme.level === 2) {
+        // 县级
+        changeResult.countyCode = itme.code
+        changeResult.countyName = itme.name
+        changeResult.fullLocation = `${changeResult.provinceName} ${changeResult.cityName} ${changeResult.countyName}`
+
+        // 最后一级
+        close()
+        emit('change', changeResult)
+      }
+    }
+    return { target, toggle, visible, currList, loading, changeItem }
+  }
+}
+// 获取城市数据
+// 1. 数据在哪里？https://yjy-oss-files.oss-cn-zhangjiakou.aliyuncs.com/tuxian/area.json
+// 2. 何时获取？打开城市列表的时候，做个内存中缓存
+// 3. 怎么使用数据？定义计算属性，根据点击的省份城市展示
+const getCityData = () => {
+  // 这个位置可能有异常操作，封装成promise
+  return new Promise((resolve, reject) => {
+    if (window.cityData) {
+      // 有缓存
+      resolve(window.cityData)
+    } else {
+      // 无缓存
+      const url = 'https://yjy-oss-files.oss-cn-zhangjiakou.aliyuncs.com/tuxian/area.json'
+      axios.get(url).then(res => {
+        window.cityData = res.data
+        resolve(window.cityData)
+      })
+    }
+  })
+}
+</script>
+<style scoped lang="less">
+.xtx-city {
+  display: inline-block;
+  position: relative;
+  z-index: 400;
+  .select {
+    border: 1px solid #e4e4e4;
+    height: 30px;
+    padding: 0 5px;
+    line-height: 28px;
+    cursor: pointer;
+    &.active {
+      background: #fff;
+    }
+    .placeholder {
+      color: #999;
+    }
+    .value {
+      color: #666;
+      font-size: 12px;
+    }
+    i {
+      font-size: 12px;
+      margin-left: 5px;
+    }
+  }
+  .option {
+    width: 542px;
+    border: 1px solid #e4e4e4;
+    position: absolute;
+    left: 0;
+    top: 29px;
+    background: #fff;
+    min-height: 30px;
+    line-height: 30px;
+    display: flex;
+    flex-wrap: wrap;
+    padding: 10px;
+    > span {
+      width: 130px;
+      text-align: center;
+      cursor: pointer;
+      border-radius: 4px;
+      padding: 0 3px;
+      &:hover {
+        background: #f5f5f5;
+      }
+    }
+  }
+}
+</style>
--- src/views/goods/components/goods-name.vue
+++ src/views/goods/components/goods-name.vue
@@ -12,7 +12,7 @@
     </dl>
     <dl>
       <dt>配送</dt>
-      <dd>至</dd>
+      <dd>至 <XtxCity @change="changeCity" :fullLocation="fullLocation" /></dd>
     </dl>
     <dl>
       <dt>服务</dt>
@@ -27,6 +27,7 @@
 </template>
 
 <script>
+import { ref } from 'vue'
 export default {
   name: 'GoodName',
   props: {
@@ -34,6 +35,33 @@ export default {
       type: Object,
       default: () => ({})
     }
+  },
+  setup (props) {
+    // 提供给后台的四项数据 (没登录)
+    const provinceCode = ref('110000')
+    const cityCode = ref('119900')
+    const countyCode = ref('110101')
+    const fullLocation = ref('北京市 市辖区 东城区')
+    // 去除用户收货地址中默认的地址给四个数据赋值
+    if (props.goods.userAddresses) {
+      const defalutAddress = props.goods.userAddresses.find(item =>
+        item.isDefault === 1
+      )
+      if (defalutAddress) {
+        provinceCode.value = defalutAddress.provinceCode
+        cityCode.value = defalutAddress.cityCode
+        countyCode.value = defalutAddress.countyCode
+        fullLocation.value = defalutAddress.fullLocation
+      }
+    }
+    // 城市选中事件处理函数
+    const changeCity = (result) => {
+      provinceCode.value = result.provinceCode
+      cityCode.value = result.cityCode
+      countyCode.value = result.countyCode
+      fullLocation.value = result.fullLocation
+    }
+    return { fullLocation, changeCity }
   }
 }
 </script>
